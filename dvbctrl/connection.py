5bdc2a2602495790e888eb9049a7810085a2da36
ccdale <chris.allison@hivehome.com>
2022-05-31 10:13:32 +0100

    initial copy and 2to3 update for connection.py

---
+++ dvbctrl/connection.py
@@ -0,0 +1,131 @@
+import socket
+import sys
+import time
+
+from dvbctrl.errors import errorNotify, DVBConnectionError, makeError
+
+
+class ControlConnection:
+    """Class implementing a connectgion to a DVBStreamer daemon."""
+
+    def __init__(self, host, adapter):
+        """Create a connection object to talk to a DVBStreamer daemon."""
+        try:
+            self.host = host
+            self.adapter = adapter
+            self.opened = False
+            self.authenticated = False
+            self.lastsuccess = 0
+        except Exception as e:
+            errorNotify(sys.exc_info()[2], e)
+
+    def expired(self):
+        try:
+            now = time.time()
+            return now > (self.lastsuccess + 5)
+        except Exception as e:
+            errorNotify(sys.exc_info()[2], e)
+
+    def open(self):
+        """Open the connection to the DVBStreamer daemon."""
+        try:
+            if self.opened:
+                return self.opened
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect((self.host, self.adapter + 54197))
+            self.authenticated = False
+            self.my_ip = self.socket.getsockname()[0]
+            self.socketfile = self.socket.makefile("r+")
+            self.opened = True
+            error_code, error_message, lines = self.read_response()
+            if error_code != 0:
+                self.socket.close()
+                self.opened = False
+            else:
+                self.welcomemsg = error_message
+            return self.opened
+        except Exception as e:
+            errorNotify(sys.exc_info()[2], e)
+
+    def close(self):
+        """Close the connection to the DVBStreamer daemon."""
+        try:
+            if self.opened:
+                self.sendCommand("logout")
+        except Exception as e:
+            errorNotify(sys.exc_info()[2], e)
+        finally:
+            if self.opened:
+                self.socketfile.close()
+                self.socket.close()
+                self.opened = False
+
+    def sendCommand(self, command):
+        """
+        Send a command to the DVBStreamer daemon connection.
+        @param command: Command to send to the server.
+        """
+        try:
+            if not self.opened:
+                raise DVBConnectionError("not connected")
+            self.socketfile.write(f"{command}\n")
+            self.socketfile.flush()
+            self.lastsuccess = time.time()
+        except Exception as e:
+            errorNotify(sys.exc_info()[2], e)
+
+    def readResponse(self):
+        """
+        Read a response from the DVBStreamer deamon after a command has been sent.
+        Returns a tuple of error code, error message and response lines.
+        """
+        try:
+            morelines = True
+            lines = []
+            errorcode = -1
+            errormessage = ""
+            while morelines:
+                line = self.socketfile.readline()
+                if line.startswith("DVBStreamer/"):
+                    morelines = False
+                    sections = line.split("/")
+                    self.version = sections[1]
+                    errorsections = sections[2].split(" ", 1)
+                    errorcode = int(errorsections[0])
+                    if len(errorsections) > 1:
+                        errormessage = errorsections[1].strip()
+                    else:
+                        errormessage = ""
+                elif line == "":
+                    morelines = False
+                else:
+                    lines.append(line.strip("\n\r"))
+            self.lastsuccess = time.time()
+            return (errorcode, errormessage, lines)
+        except Exception as e:
+            errorNotify(sys.exc_info()[2], e)
+
+    def executeCommand(self, command):
+        """
+        Send command and wait for response
+        Returns a tuple of error message and response lines if the return error code was 0,
+        otherwise a DVBStreamerError is raised.
+        """
+        try:
+            self.sendCommand(command)
+            errorcode, errormessage, lines = self.readResponse()
+            if errorcode != 0:
+                raise makeError(errorcode, errormessage, lines)
+            return (errormessage, lines)
+        except Exception as e:
+            errorNotify(sys.exc_info()[2], e)
+
+    def authenticate(self, username, password):
+        """
+        Authenticate the connection allowing it to execute more commands.
+        """
+        try:
+            errormessage, lines = self.executeCommand(f'auth "{username}" "{password}"')
+            self.authenticated = True
+        except Exception as e:
+            errorNotify(sys.exc_info()[2], e)
--- dvbctrl/errors.py
+++ dvbctrl/errors.py
@@ -20,3 +20,52 @@ def errorRaise(exci, e):
 def errorExit(exci, e):
     errorNotify(exci, e)
     sys.exit(1)
+
+
+class DVBConnectionError(Exception):
+    pass
+
+
+class DVBStreamerError(Exception):
+    def __init__(self, code, msg, lines=None):
+        self.code = code
+        self.msg = msg
+        self.lines = lines
+
+    def __str__(self):
+        return f"{self.code}: {self.msg}"
+
+
+class TooManyConnectionsError(DVBStreamerError):
+    pass
+
+
+class UnknownCommandError(DVBStreamerError):
+    pass
+
+
+class WrongArgumentsError(DVBStreamerError):
+    pass
+
+
+class NotAuthenticatedError(DVBStreamerError):
+    pass
+
+
+class GenericCommandError(DVBStreamerError):
+    pass
+
+
+def makeError(code, msg, lines=None):
+    try:
+        if code == 1:
+            return TooManyConnectionsError(code, msg, lines)
+        elif code == 2:
+            return UnknownCommandError(code, msg, lines)
+        elif code == 3:
+            return WrongArgumentsError(code, msg, lines)
+        elif code == 4:
+            return NotAuthenticatedError(code, msg, lines)
+        return GenericCommandError(code, msg, lines)
+    except Exception as e:
+        errorNotify(sys.exc_info()[2], e)
