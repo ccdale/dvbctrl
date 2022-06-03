b1396fa29416daeaa2cdee67d4ef2650427d265f
ccdale <chris.allison@hivehome.com>
2022-06-03 12:07:18 +0100

    fix

--- dvbctrl/dvbv5.py
+++ dvbctrl/dvbv5.py
@@ -9,11 +9,14 @@ import sys
 from dvbctrl.errors import errorNotify
 from dvbctrl.shell import shellCommand
 
-class Recorder():
+
+class Recorder:
     def __init__(self, adapter=0, basefd=None):
         try:
             self.adapter = adapter
-            self.basefd = Path(basefd) if basefd else Path("/run/media/chris/seagate4/TV/tv")
+            self.basefd = (
+                Path(basefd) if basefd else Path("/run/media/chris/seagate4/TV/tv")
+            )
             self.basefd.mkdir(parents=True, exist_ok=True)
             self.fqfn = None
         except Exception as e:
@@ -30,10 +33,10 @@ class Recorder():
             ftitle = self.title.replace(" ", "_")
             dt = datetime.fromtimestamp(start)
             fn = f"{dt.year}{dt.month:0>2}{dt.day:0>2}T{dt.hour:0>2}{dt.min:0>2}"
-            fn +=f"-{fchan}-{ftitle}.ts"
+            fn += f"-{fchan}-{ftitle}.ts"
             self.fdir = Path(self.basefd / ftitle)
             self.fdir.mkdir(parents=True, exist_ok=True)
-            self.fqfn = Path(self.fdir / fn])
+            self.fqfn = Path(self.fdir / fn)
         except Exception as e:
             errorNotify(sys.exc_info()[2], e)
 
