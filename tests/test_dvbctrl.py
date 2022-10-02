30988aa8edfd2cbb7f0ebe57719624a985951a19
ccdale <chris.charles.allison@gmail.com>
2022-10-02 12:56:57 +0100

    ok, all tests in one file, sigh

--- tests/test_dvbctrl.py
+++ tests/test_dvbctrl.py
@@ -1,5 +1,36 @@
+import pytest
+
 from dvbctrl import __version__
+from dvbctrl.commands import DVBCommand
+from dvbctrl.connection import ControlConnection
+from dvbctrl.dvbstreamer import DVBStreamer
+
+
+@pytest.fixture
+def dvbobj(scope="module"):
+    dvb = DVBStreamer(0)
+    dvb.start()
+    yield dvb
+    dvb.stop()
 
 
 def test_version():
     assert __version__ == "0.2.5"
+
+
+def test_connect(dvbobj):
+    # dvb = DVBStreamer(0)
+    # dvb.start()
+    cc = ControlConnection(0)
+    cc.open()
+    assert cc.opened == True
+    assert cc.welcomemsg == "Ready"
+
+
+def test_lslcn(dvbobj):
+    # dvb = DVBStreamer(0)
+    # dvb.start()
+    dvbc = DVBCommand()
+    lines = dvbc.lslcn()
+    assert lines == "hello"
+    # dvb.stop()
