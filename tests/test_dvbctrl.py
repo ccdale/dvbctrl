bb763347c474414210356488359986c1e382e578
ccdale <chris.charles.allison@gmail.com>
2022-10-08 04:12:54 +0100

    exit the tests if dvbstreamer fails to start

--- tests/test_dvbctrl.py
+++ tests/test_dvbctrl.py
@@ -1,19 +1,25 @@
 import pytest
+import sys
 import time
 
 from dvbctrl import __version__
 from dvbctrl.commands import DVBCommand
 from dvbctrl.connection import ControlConnection
 from dvbctrl.dvbstreamer import DVBStreamer
+from dvbctrl.errors import errorExit
 
 
 @pytest.fixture(scope="module")
 def dvbobj():
-    dvb = DVBStreamer(0)
-    dvb.start()
-    # time.sleep(1)  # give it some time to settle
-    yield dvb
-    dvb.stop()
+    try:
+        dvb = DVBStreamer(0)
+        running = dvb.start()
+        if not running:
+            raise Exception("Failed to start dvbstreamer on adaptor 0")
+        yield dvb
+        dvb.stop()
+    except Exception as e:
+        errorExit(sys.exc_info()[2], e)
 
 
 def test_version():
